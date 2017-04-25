bf03e0beccc5d71c5c2b48cc32928c2260035644
tbadman <tbadman@gmail.com>
2017-04-25 16:14:02 -0400

    population tracking

---
+++ my_class.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class GameData:
+
+    def __init__(self,colors):
+        self.line, = plt.plot([],[],color=next(colors))
+        self.name = ''
+        self.viewers = 0
+        self.text = ''
+        self.text_xpos = 0
+        self.text_ypos = 0
+        self.text_color = None
+        self.streamer = ''
+        self.stream_viewers = 0
+
+    def set_name(self,name):
+        self.name = name
+
+    def update_text(self, x,y):
+        self.text = ("Game: " + self.name + "\n" +
+                     "Viewers: " + str(self.viewers) + "\n" +
+                     "Top Streamer: " + self.streamer + "\n" +
+                     "Stream Viewers: " + str(self.stream_viewers))
+        self.text_xpos = x
+        self.text_ypos = y - 20000
+        self.text_color = self.line.get_color()
+
+    def update_data(self,x,y,reset):
+        self.viewers = y
+        self.line.set_xdata(np.append(self.line.get_xdata(),x))
+        if not reset:
+            self.line.set_ydata(np.append(self.line.get_ydata(),y))
+        else:
+            self.line.set_ydata(
+                np.append([None for m in range(len(self.line.get_ydata()))],y)
+            )
+
+    def set_streamer_data(self, streamer, viewers):
+        self.streamer = streamer
+        self.stream_viewers = viewers
--- population_tracking.py
+++ population_tracking.py
@@ -1,92 +1,115 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from my_class import GameData
+import os
 import matplotlib.cm as cm
 import numpy as np
 import requests
 import requests_cache
 import json
 
-url = "https://api.twitch.tv/kraken/games/top"
+games_url = "https://api.twitch.tv/kraken/games/top"
 clientID = {"Client-ID": "XXXXX"}
+
+try:
+    os.remove('twitch_cache.sqlite')
+except OSError:
+    pass
 
 requests_cache.install_cache('twitch_cache', backend='sqlite', expire_after=10)
 
-refresh_rate = 200 #ms
+def request_game_data():
+    
+    games = requests.get(games_url,headers=clientID)
+    games = games.json()
+
+    return games
+
+def request_stream_data(game):
+
+    stream_url = "https://api.twitch.tv/kraken/streams/?game=" + game
+    stream = requests.get(stream_url,headers=clientID)
+    stream = stream.json()
+
+    return stream
+
+def init():
+    
+    game_data = request_game_data()
+    for i,game in enumerate(games):
+        game_name = game_data['top'][i]['game']['name']
+        streamers = request_stream_data(game_name)
+        
+        top_streamer = streamers['streams'][0]['channel']['display_name']
+        top_streamer_viewers = streamers['streams'][0]['viewers']
+        
+        game.set_name(game_name)
+        game.set_streamer_data(top_streamer,top_streamer_viewers)
+        
+    return games
+
+def draw_tooltip(game):
+    
+    tooltip.set_text(game.text)
+    tooltip.set_position(ax.transLimits.transform((game.text_xpos, game.text_ypos)))
+    tooltip.set_backgroundcolor(game.text_color)
+
+def on_plot_hover(event, games):
+    
+    if event.xdata is not None and event.ydata is not None:
+        for j,game in enumerate(games):
+            for i in range(len(game.line.get_xdata())):
+                if (abs(event.xdata - game.line.get_xdata()[i]) < 10 and
+                    abs(event.ydata - game.line.get_ydata()[i]) < 1000):
+                    game.update_text(event.xdata,event.ydata)
+                    draw_tooltip(game)
+                else:
+                    game.update_text(0,0)
+                    
+                
+def animate(i):
+    
+    game_data = request_game_data()
+    curr_time = float(i)*float(refresh_rate)/1000.
+
+    old_order = [game.name for game in games]
+    new_order = [new_game['game']['name'] for new_game in game_data['top']]
+    
+    for game in games:
+        
+        try:
+            game.update_data(curr_time,game_data['top'][new_order.index(game.name)]['viewers'],0)
+        except:
+            for new_game in game_data['top']:
+                if new_game['game']['name'] not in old_order:
+                    print 'NEW GAME: ' + new_game['game']['name']
+                    game.set_name(new_game['game']['name'])
+                    game.update_data(curr_time,new_game['viewers'],1)
+
+        streamers = request_stream_data(game.name)
+        top_streamer = streamers['streams'][0]['channel']['display_name']
+        top_streamer_viewers = streamers['streams'][0]['viewers']
+        game.set_streamer_data(top_streamer,top_streamer_viewers)
+                    
+    if curr_time>300:
+        ax.set_xlim(curr_time+0.0001-300,curr_time)
+    else:
+        ax.set_xlim(0,curr_time+0.0001)
+    ax.set_ylim(game_data['top'][9]['viewers']-10000,game_data['top'][0]['viewers']+10000)
+    
+    return games
+
+refresh_rate = 400 #ms
 colors = iter(cm.rainbow(np.linspace(0, 1, 10)))
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 100), ylim=(0, 120000))
-lines = [plt.plot([], [], color = next(colors), label = 'line {}'.format(i))[0] for i in range(10)]
+tooltip = ax.text(0, 0,  '', fontsize=8,bbox={'facecolor':'white', 'alpha':0.5}, transform = ax.transAxes)
 plt.xlabel("Seconds")
 plt.ylabel("Number of Viewers")
 plt.title("Most Popular Twitch Streams")
 
-def init():    
-    for line in lines:
-        line.set_data([], [])
-    global texts
-    texts = [ax.text(0, 0.95-i*0.05,  '', fontsize=8,
-                     bbox={'facecolor':'white', 'alpha':0.5}) for i in range(len(lines))]
-    return lines
-    
-def update_text(text,new_info,new_time,line):
-    
-    text.set_text(new_info['game']['name'])
-    text.set_x(text.get_x+refresh_rate/1000)
-    text.set_y(new_info['viewers'])
-    text.set_backgroundcolor(line.get_color())
-    
-    return text
-
-def on_plot_hover(event):
-    for j,line in enumerate(lines):
-        for i in range(len(line.get_xdata())):
-            if (event.xdata is not None and event.ydata is not None and
-                abs(float(event.xdata) - float(line.get_xdata()[i])) < 10 and abs(event.ydata - line.get_ydata()[i]) < 1000):
-
-                for k in range(len(lines)):
-                    texts[k].set_text('')
-                    texts[k].set_x(0)
-                    texts[k].set_y(0)
-                    
-                texts[j].set_text("Game: " + str(data['top'][j]['game']['name'])+"\n"+"Viewers: " + str(data['top'][j]['viewers']))
-                texts[j].set_position((event.xdata,event.ydata-20000))
-                texts[j].set_backgroundcolor(line.get_color())
-                
-def animate(i):
-    r = requests.get(url,headers=clientID)
-    global data
-    data = r.json()
-    time_seconds = float(i)*float(refresh_rate)/1000
-    
-    if i==0:
-        global order
-        order = [data['top'][n]['game']['name'] for n in range(10)]
-        #ax.legend(labels=[data['top'][j]['game']['name'] for j in range(10)], loc="upper left", fontsize='x-small')
-            
-    for j,line in enumerate(lines):
-        line.set_xdata(np.append(line.get_xdata(),time_seconds))
-        
-        game_found = False
-        for top_game in data['top']:
-            if top_game['game']['name'] == order[j]:
-                line.set_ydata(np.append(line.get_ydata(), top_game['viewers']))
-                game_found = True
-        if not game_found:
-            for top_game in data['top']:
-                if top_game['game']['name'] not in order:
-                    print('NEW GAME: ',top_game['game']['name'])
-                    order[j] = top_game['game']['name']
-                    line.set_ydata(np.append([None for m in range(len(line.get_ydata()))],top_game['viewers']))
-                    #update_text(texts[j],top_game,time_seconds,line)
-                    
-    if time_seconds>300:
-        ax.set_xlim(time_seconds+0.0001-300,time_seconds)
-    else:
-        ax.set_xlim(0,time_seconds+0.0001)
-    ax.set_ylim(data['top'][9]['viewers']-10000,data['top'][0]['viewers']+10000)
-
-    return lines
+games = [GameData(colors) for _ in range(10)]
 
 anim = animation.FuncAnimation(fig, animate, init_func=init, interval=refresh_rate, blit=False)
-fig.canvas.mpl_connect('motion_notify_event', on_plot_hover)
+fig.canvas.mpl_connect('motion_notify_event', lambda event: on_plot_hover(event, games))
 plt.show()
